5e9b88db7170e57e5c4265c5ed43a79dd6fb21b7
bharath2023ht <126450752+bharath2023ht@users.noreply.github.com>
2025-09-01 23:03:42 +0530

    'update'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { HomePageComponent } from './common/home-page/home-page.component';
+import { FinalPageComponent } from './common/final-page/final-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    FinalPageComponent
   ],
   imports: [
     BrowserModule,
