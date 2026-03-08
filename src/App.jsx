0c7d4bbc5394e6f01c5cd34ef0002ab330dcd4c8
Gandheesh-SA <sagandheesh1911@gmail.com>
2026-03-08 10:06:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 
-import HomePage from "./pages/Homepage";
+import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ResearchPage from "./pages/ResearchPage";
 import CorporatePage from "./pages/CorporatePage";
